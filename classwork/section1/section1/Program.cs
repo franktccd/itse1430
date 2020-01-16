95dadb5078b1ed29744661b9e6985f3808f0a700
franktccd <frank.rygiewicz@my.tccd.edu>
2020-01-15 21:38:52 -0600

    Playing with syntax

--- classwork/section1/section1/Program.cs
+++ classwork/section1/section1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ * ITSE 1430
+ * Frank Rygiewicz
+ */
+ using System;
 
 namespace section1
 {
@@ -7,6 +11,31 @@ namespace section1
         static void Main ( string[] args )
         {
             Console.WriteLine("Hello World!");
+
+            int hours;
+            double payRate;
+            string name;
+            bool pass;
+
+            /*int newHours = hours;*/ //Won't compile because hours has not been defined
+            hours = 10;
+            int newHours2 = hours;
+
+            //Logical block 1
+            int hours2 = 10;
+            int hours3; //Don't do this
+            hours3 = 3; //Don't so this
+
+            //int results; //Don't do this
+            //results = Foo(); //Don't do this
+            //int results2 = Foo();
+
+            int x, y, z;
+            int a = 10, b = 20, c = 30;
+
+
+
+            //Logical block 2
         }
     }
 }
