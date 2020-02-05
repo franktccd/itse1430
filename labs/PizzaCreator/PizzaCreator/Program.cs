8313b3f1ae5875c465655b28b9ab1c53e69e2091
Frank Rygiewicz <frank.rygiewicz@my.tccd.edu>
2020-02-05 12:22:19 -0600

    Created PizzaCreator solution in github

---
+++ labs/PizzaCreator/PizzaCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PizzaCreator for C# Programming ITSE 1430-21722 by Frank Rygiewicz");
+        }
+    }
+}
